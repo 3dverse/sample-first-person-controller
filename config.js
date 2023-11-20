0190a413b060397b4fedaa81fb896a88566912cc
Houssem <houssem@3dverse.com>
2023-11-20 17:14:28 -0500

    feat: demo

--- config.js
+++ config.js
@@ -2,15 +2,15 @@
 // - Go to the API Access page.
 // - Copy the Public Token and paste it below.
 
-export const publicToken = %YOUR_PUBLIC_TOKEN%;
+export const publicToken = "public_ZfIxTOzgSYnoHzWh";
 
 // Replace asset UUIDs:
 // - Go to the Asset Browser page.
 // - Double click on the Public folder to open it.
 // - Select a scene and copy the Asset UUID to replace it below.
 
-export const mainSceneUUID = %YOUR_MAIN_SCENE_UUID%;
-export const characterControllerSceneUUID = %YOUR_CHARACTER_CONTROLLER_SCENE_UUID%;
+export const mainSceneUUID = "db2319f4-9f77-4215-93b9-59e7ac101324";
+export const characterControllerSceneUUID = "eb7d4a9e-1354-41ba-abbe-a9529894b84b";
 
 
 export const spawnPosition = [0,0,0];
