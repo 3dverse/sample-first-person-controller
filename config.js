133cd47e723ea3bd7c323c1ed037e65cda11521a
Mark Downey <mark@thebakery.io>
2024-01-30 19:35:58 +0000

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,13 @@
+// Find your Public Token in the 3dverse Console (https://console.3dverse.com):
+// - Go to the API Access page.
+// - Copy the Public Token and paste it below.
+
 export const publicToken = %YOUR_PUBLIC_TOKEN%;
+
+// Replace asset UUIDs:
+// - Go to the Asset Browser page.
+// - Double click on the Public folder to open it.
+// - Select a scene and copy the Asset UUID to replace it below.
+
 export const mainSceneUUID = %YOUR_MAIN_SCENE_UUID%;
 export const characterControllerSceneUUID = %YOUR_CHARACTER_CONTROLLER_SCENE_UUID%;
