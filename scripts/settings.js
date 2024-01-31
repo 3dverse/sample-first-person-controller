730d520737803e7d0d63e77811ff4484764d2a64
Gabriel <gabriel@3dverse.com>
2024-01-31 01:36:42 +0100

    Apply coding conventions, rename variables, fix camera orientation reset, remove actionmap interactions

--- scripts/settings.js
+++ scripts/settings.js
@@ -1,37 +1,121 @@
 //------------------------------------------------------------------------------
 import {
     characterControllerSceneUUID,
-    characterControllerScriptUUID,
-    clientData
 } from "../config.js";
 
-import { LockPointer } from "./utils.js";
+import {
+    LockPointer,
+    RecoverPitchAndYawFromQuat,
+    UnlockPointer
+} from "./utils.js";
 
-window.EditControl = EditControl;
-window.CancelControlKeyEdit = CancelControlKeyEdit;
-var specialKeys = {
-    " ": "SPACE",
-    "\r": "ENTER",
-    "\b": "BACKSPACE",
-    "\t": "TAB",
-    "\x10": "SHIFT",
-    "\x11": "CTRL",
-    "\x12": "ALT",
-    "\x14": "CAPSLOCK",
-    "&": "ARROW UP",
-    "(": "ARROW DOWN",
-    "%": "ARROW LEFT",
-    "'": "ARROW RIGHT",
+//------------------------------------------------------------------------------
+var device = "mouse";
+const PHYSICAL_ACTION_KEYS = {
+    "MOVE_FORWARD": ["KeyW"],
+    "MOVE_BACKWARD": ["KeyS"],
+    "MOVE_LEFT": ["KeyA"],
+    "MOVE_RIGHT": ["KeyD"],
+    "DANCE": ["KeyE"],
+    "JUMP": ["SPACE"],
+    "SPRINT": ["SHIFT"],
+}
+
+// If the browser supports keyboard layout detection, we display layout based 
+// keys instead of physical keys.
+if ('keyboard' in navigator && 'getLayoutMap' in navigator.keyboard) {
+    GetActionKey = GetLayoutBasedActionKey;
 }
 
 //------------------------------------------------------------------------------
-export async function InitDeviceDetection() {
-    if(navigator.getGamepads().length > 0) {
-        await SetDevice('gamepad');
-        ResetMouseDetection();
-    } else {
-        ResetGamepadDetection();
+function GetActionKey(physicalActionKey) {
+    return physicalActionKey.replace("Key", "");
+}
+
+//------------------------------------------------------------------------------
+async function GetLayoutBasedActionKey(physicalActionKey) {
+    // For none layout affected keys, (Space, Shift, etc.)
+    if(!physicalActionKey.includes("Key")) {
+        return physicalActionKey;
     }
+
+    const keyboardLayoutMap = await navigator.keyboard.getLayoutMap()
+    const layoutActionKey = keyboardLayoutMap.get(physicalActionKey)
+    return layoutActionKey ? layoutActionKey.toUpperCase() : "UNKNOWN";
+}
+
+//------------------------------------------------------------------------------
+export async function InitControlKeySettings() {
+    let actionKeysElements = document.getElementsByClassName("action-keys");
+    let action, displayedKeys;
+    Array.from(actionKeysElements).forEach(async (element) => {
+        action = element.getAttribute("data-action")     
+        displayedKeys = '';
+        for (const key of PHYSICAL_ACTION_KEYS[action]) {
+            displayedKeys += (await GetActionKey(key)) + " + ";
+        }
+        displayedKeys = displayedKeys.slice(0, -3);
+        element.innerHTML = displayedKeys;
+    });
+}
+
+//------------------------------------------------------------------------------
+export async function AdjustDeviceSensitivity() {
+    // We recover the device and the sensitivity from the settings modal inputs.
+    const sensitivitySetting = GetSensitivity();
+    
+    // We adjust the sensitivity depending on the device. The joysticks on 
+    // gamepads produce less sensitive values than the mouse. The new 
+    // sensitivity for gamepad has to be higher. Around ~0.x sensitivity for 
+    // mouse and ~x.0 for gamepad within the current asset script for character 
+    // controller camera management.
+    let newSensitivity;
+    if ( device ===  "gamepad" ){
+        newSensitivity = sensitivitySetting / 5;
+    } else { //if ( device === "mouse" ){
+        newSensitivity = sensitivitySetting / 100;
+    }
+
+    // By default, the camera orientation is reset when calling 
+    // "setScriptInputValues()".  We pass the current pitch and yaw of the 
+    // camera to the Asset Script to keep the camera orientation.
+    // Camera orientation can be obtained from the viewport orientation.
+    const activeViewports = SDK3DVerse.engineAPI.cameraAPI.getActiveViewports();
+    const viewportOrientation = activeViewports[0].getTransform().orientation;
+    const { pitch, yaw } = RecoverPitchAndYawFromQuat(viewportOrientation);
+
+    // We update the inputs of the Asset Script attached to the character 
+    // controller, specifically the sensitivity. Asset Scripts inputs are 
+    // accessed through the "script_map" component of an entity.
+    const characterControllerScriptUUID = Object.keys(window.characterController.getComponent("script_map").elements)[0];
+    window.characterController.setScriptInputValues(
+        characterControllerScriptUUID, 
+        {
+            pitch: pitch,
+            yaw: yaw,
+            sensitivity: newSensitivity,
+        }
+    );
+
+    // This should be removed when setScriptInputValues() is fixed, it's just 
+    // that since setScriptInputValues doesn't return a promise, we must make 
+    // sure that the script inputs have been updated before reassignign client 
+    // to script.
+    await SDK3DVerse.engineAPI.findEntitiesByEUID(characterControllerSceneUUID); 
+    await new Promise((resolve) => setTimeout(resolve, 100));
+    // Reassign the client's inputs to the Asset Script of the character 
+    // controller
+    SDK3DVerse.engineAPI.assignClientToScripts(window.characterController);  
+}
+
+//------------------------------------------------------------------------------
+function GetSensitivity() {
+    return document.getElementById("sensitivity-slider").value;
+}
+
+//------------------------------------------------------------------------------
+export function InitDeviceDetection() {
+    ResetGamepadDetection();
 }
 
 //------------------------------------------------------------------------------
@@ -39,8 +123,9 @@ async function ResetGamepadDetection() {
     window.addEventListener(
         'gamepadconnected', 
         async () => {
-            await SetDevice('gamepad');
+            device = 'gamepad';
             AdjustDeviceSensitivity();
+            LockPointer();
             ResetMouseDetection();
         }, 
         { once: true } 
@@ -52,232 +137,13 @@ async function ResetMouseDetection() {
     window.addEventListener(
         'mousemove', 
         async ()=> { 
-            await SetDevice('mouse');
+            device = 'mouse';
             AdjustDeviceSensitivity();
+            UnlockPointer();
             ResetGamepadDetection();
         },
         { once: true }
     );
-}
-
-//------------------------------------------------------------------------------
-export async function AdjustDeviceSensitivity() {
-    // We recover the device and the sensitivity from the settings modal inputs.
-    const device = GetDevice();
-    const sensitivitySetting = GetSensitivity();
-    
-    // We adjust the sensitivity depending on the device. The joysticks on 
-    // gamepads produce less sensitive values than the mouse. The new 
-    // sensitivity for gamepad has to be higher. Around ~0.x sensitivity for 
-    // mouse and ~x.0 for gamepad within the current asset script for character 
-    // controller camera management.
-    if ( device ==  "gamepad" ){
-        var newSensitivity = sensitivitySetting / 5;
-    } else { //if ( device == "mouse" ){
-        var newSensitivity = sensitivitySetting / 100;
-    }
-
-    // We update the inputs of the Asset Script attached to the character 
-    // controller, specifically the sensitivity. Asset Scripts inputs are 
-    // accessed through the "script_map" component of an entity.
-    clientData.characterController.setScriptInputValues(
-        characterControllerScriptUUID, 
-        { sensitivity: newSensitivity }
-    );
-
-    // This should be removed when setScriptInputValues() is fixed, it's just 
-    // that since setScriptInputValues doesn't return a promise, we must make 
-    // sure that the script inputs have been updated before reassignign client 
-    // to script.
-    await SDK3DVerse.engineAPI.findEntitiesByEUID(characterControllerSceneUUID); 
-
-    // Reassign the client's inputs to the Asset Script of the character 
-    // controller
-    SDK3DVerse.engineAPI.assignClientToScripts(clientData.characterController);  
-}
-
-//------------------------------------------------------------------------------
-export function GetSensitivity() {
-    return document.getElementById("sensitivity-slider").value;
-}
-
-//------------------------------------------------------------------------------
-export function GetDevice() {
-    return document.getElementById("device").value;
-}
-
-//------------------------------------------------------------------------------
-export async function SetDevice(device) {
-    document.getElementById("device").value = device;
-}
-
-//------------------------------------------------------------------------------
-export async function EditControl(actionButton) {
-    ShowControlKeyEditModal();
-    
-    let controlsCaptureTimeout = setTimeout(() => { 
-        controlsCaptureTimeout = null;
-        CloseControlKeyEditModal();
-    }, 5 * 60 * 1000);
-
-    // The user edits the keys of an action by pressing keys.
-    document.addEventListener('keydown', (event) => {
-        HandleKeyDownControlEdit(event, actionButton, controlsCaptureTimeout)
-    }, { once: true });
-}
-
-//------------------------------------------------------------------------------
-async function HandleKeyDownControlEdit(event, element, controlsCaptureTimeout, newActionKeys=[]) {
-    // If the timeout is null, it means that the timeout has been executed and
-    // that the key edition session is over.
-    if( !controlsCaptureTimeout ) {
-        return;
-    }
-
-    // We stop the timeout that saves the key edition after a period of time 
-    // since the user desires to change his key selection.
-    clearTimeout(controlsCaptureTimeout);
-
-    // If escape is pressed (or triggered through the cancel button), we close
-    // the key editing popup without saving the changes.
-    if( event.key == "Escape" ){
-        CloseControlKeyEditModal();
-        return;
-    }
-
-    const newKey =  "KEY_" + getLayoutAgnosticKeyCode(event);
-    // If a new key is assigned to the action, we add it to the list of keys
-    // and display it in the key editing popup.
-    if(!(newActionKeys.includes(newKey))) {
-        newActionKeys.push(newKey);
-        document.getElementById("control-key-input").innerHTML = await generateActionKeysDisplay(newActionKeys);
-    }
-
-    // We reset the timeout to let more time to the user to add more keys.
-    controlsCaptureTimeout = setTimeout(() => { 
-        // Setting the timeout to null allows to know if the timeout has been 
-        // executed when HandleKeyDownControlEdit() is called on a keydown event 
-        // with the timeout variable as argument. controlsCaptureTimeout will
-        // be equal to null.
-        controlsCaptureTimeout = null;
-        replaceActionKeys(element, newActionKeys);
-        CloseControlKeyEditModal();
-    }, 3 * 1000);
-
-    // We wait for the next keydown event to continue the key edition and pass
-    // controlsCaptureTimeout as argument to detect on the next keydown if the
-    // key edition session is over. 
-    document.addEventListener('keydown', (event) => { 
-        HandleKeyDownControlEdit(
-            event, 
-            element, 
-            controlsCaptureTimeout, 
-            newActionKeys)
-        },
-        { once: true });
-} 
-
-//------------------------------------------------------------------------------
-async function replaceActionKeys(element, newActionKeys) {
-    if(newActionKeys.length == 0) {
-        return;
-    }
-
-    const editingAction = element.getAttribute("data-action");
-    const editingIndex = element.getAttribute("data-keys-id");
-    SDK3DVerse.actionMap.values[editingAction][editingIndex] = newActionKeys;
-    SDK3DVerse.actionMap.propagate();
-
-    element.innerHTML = await generateActionKeysDisplay(newActionKeys);
-}
-
-//------------------------------------------------------------------------------
-async function generateActionKeysDisplay(actionKeys) {
-    let keysString = '';
-    for (const key of actionKeys) {
-        keysString += (await getLayoutKeyFromActionKey(key)) + " + ";
-    }
-    keysString = keysString.slice(0, -3);
-    // If multiple keys, keyString will be "Z + Q + ARROW UP" for example and 
-    // non keyboard agnostic.
-    return keysString;
-}
-
-//------------------------------------------------------------------------------
-async function getLayoutKeyFromActionKey(actionKey) {
-    const key = String.fromCharCode(parseInt(actionKey.replace("KEY_", '')));
-    if(Object.keys(specialKeys).includes(key)) {
-        return specialKeys[key];
-    }
-
-    const numericKey = parseInt(key);
-    if(!isNaN(numericKey)) {
-        return numericKey;
-    }
-    return (await getNonLayoutAgnosticKey("Key" + key)).toUpperCase();
-}
-
-//------------------------------------------------------------------------------
-function getLayoutAgnosticKeyCode(event)
-{
-    const { code, key } = event;
-    if(!code?.startsWith('Key')) {
-        return event.keyCode;
-    }
-    const keyFromCode = code[code.length - 1];
-    if(keyFromCode !== key) {
-        return keyFromCode.charCodeAt(0);
-    }
-    return event.keyCode;
-}
-
-//------------------------------------------------------------------------------
-async function getNonLayoutAgnosticKey(layoutAgnosticKey) {
-    const keyboardLayoutMap = await navigator.keyboard.getLayoutMap()
-    const nonLayoutAgnosticKey = keyboardLayoutMap.get(layoutAgnosticKey);
-    return nonLayoutAgnosticKey ? nonLayoutAgnosticKey : "Unknown Key";
-}
-
-//------------------------------------------------------------------------------
-export async function LoadControlKeySettings() {
-    let keyValueHolders = document.getElementsByClassName("edit-action-keys-button");
-    let action, index, keys;
-    Array.from(keyValueHolders).forEach(async (element) => {
-        action = element.getAttribute("data-action")
-        index = element.getAttribute("data-keys-id")
-        keys = SDK3DVerse.actionMap.values[action][parseInt(index)];
-        element.innerHTML = await generateActionKeysDisplay(keys);
-    });
-}
-
-//------------------------------------------------------------------------------
-async function CancelControlKeyEdit() {
-    let event = new KeyboardEvent('keydown', {
-        key: 'Escape',
-        keyCode: 27,
-        which: 27,
-        code: 'Escape',
-      });
-    
-    // Dispatching the event instead of closing the modal directly allows to 
-    // clean the keydown event listeners related to the control keys edition in 
-    // HandleKeyDownControlEdit().
-    document.dispatchEvent(event);
-}
-
-//------------------------------------------------------------------------------
-async function ShowControlKeyEditModal() {
-    const editActionModal = document.getElementById("control-key-editor")
-    editActionModal.parentNode.classList.add('active');
-}
-
-//------------------------------------------------------------------------------
-async function CloseControlKeyEditModal() {
-    const editActionModal = document.getElementById("control-key-editor")
-    editActionModal.parentNode.classList.remove('active');
-
-    const editActionModalInput = document.getElementById("control-key-input")
-    editActionModalInput.innerHTML = "";
 }
 
 //------------------------------------------------------------------------------
@@ -293,10 +159,7 @@ export async function CloseSettingsModal() {
     const settingsContainer = document.getElementById("settings-modal").parentNode;
     settingsContainer.classList.remove('active');
     AdjustDeviceSensitivity();
-    try {
-        await LockPointer();
-        SDK3DVerse.enableInputs(); 
-    } catch (error) {
-        console.error("Pointer lock not available");
-    }
+    SDK3DVerse.enableInputs();
+    const close = document.getElementById("close");
+    close.removeEventListener('click', CloseSettingsModal); 
 }
--- scripts/utils.js
+++ scripts/utils.js
@@ -1,14 +1,50 @@
 //------------------------------------------------------------------------------
 export async function LockPointer() {
     const canvas = document.getElementById("display-canvas");
+    canvas.removeEventListener('mousedown', LockPointer);
+
     canvas.requestPointerLock = (
-      canvas.requestPointerLock 
-      || canvas.mozRequestPointerLock 
-      || canvas.webkitPointerLockElement
+        canvas.requestPointerLock 
+        || canvas.mozRequestPointerLock 
+        || canvas.webkitPointerLockElement
     );
     await canvas.requestPointerLock();
     // Focusing the canvas is necessary to make user's inputs work since most
-    // user inputs are detected through Web "onkeydown" Event Listeners attached
-    // to the canvas.
+    // user inputs are detected through Web "onkeydown" Event Listeners
+    // attached to the canvas.
     canvas.focus();
+    canvas.addEventListener('mouseup', UnlockPointer);
 }
+
+//------------------------------------------------------------------------------
+export async function UnlockPointer() {
+    const canvas = document.getElementById("display-canvas");
+    canvas.removeEventListener('mouseup', UnlockPointer);
+    
+    document.exitPointerLock = (
+      document.exitPointerLock 
+        || document.mozExitPointerLock 
+        || document.webkitExitPointerLock
+    );
+    await document.exitPointerLock();
+    canvas.addEventListener('mousedown', LockPointer);
+}
+
+//------------------------------------------------------------------------------
+export function RecoverPitchAndYawFromQuat(quaternion) {
+    const eulerAngle = SDK3DVerse.utils.quaternionToEuler(quaternion);
+
+    let yaw, pitch;
+    if((eulerAngle[2] < 90) && (eulerAngle[2] > -90)) {
+        pitch = eulerAngle[0];
+        yaw = 180 + eulerAngle[1];
+    } else {
+        if(eulerAngle[0] > 0) {
+            pitch = eulerAngle[0] - 180;
+        } else {
+            pitch = eulerAngle[0] + 180;
+        }
+        yaw = -eulerAngle[1];
+    }
+    return {yaw:yaw, pitch:pitch}
+  }
